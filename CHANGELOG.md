b7dc0aa587999d3882f966cdf3dc5329e6b43170
Ryan Kotzen <potz666@gmail.com>
2017-08-01 21:40:25 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Change Log
+
+## [Unreleased](https://github.com/krimzenNinja/krimzen-ninja-logging/compare/v0.0.2...HEAD)
+
+Unreleased features to be listed here
+
+## [0.0.2](https://github.com/krimzenNinja/krimzen-ninja-logging/compare/v0.0.1...v0.0.2) (2017-07-22)
+
+* Testing out if the publish step works.
+
+## 0.0.1 (2017-07-22)
+
+* initial release of empty project.
---
+++ src/index.js
@@ -0,0 +1,95 @@
+// @flow
+
+const winston = require('winston');
+const packageJson = require('../../package.json');
+require('winston-daily-rotate-file');
+const WinstonGraylog2 = require('winston-graylog2');
+const config = require('krimzen-ninja-config');
+const formatArgs = require('./format-args');
+const _ = require('lodash');
+const logSettings = config.get('logging');
+const fs = require('fs');
+const path = require('path');
+const logger = new winston.Logger({ exitOnError: true });
+addConsoleLogging();
+addFileLogging();
+addLogglyLogging();
+addGraylogLogging();
+overrideConsole();
+
+function addConsoleLogging() {
+    const consoleSettings = logSettings.console;
+    if (consoleSettings.disabled) {
+        return;
+    }
+    logger.add(winston.transports.Console, _.omit(consoleSettings, 'disabled'));
+}
+
+function addFileLogging() {
+    const fileSettings = logSettings.file;
+    if (fileSettings.disabled) {
+        return;
+    }
+    const folder = path.dirname(fileSettings.datePattern);
+    ensureLogFolderExists(folder);
+    logger.add(winston.transports.DailyRotateFile, _.omit(fileSettings, 'disabled'));
+}
+function ensureLogFolderExists(folder) {
+    if (!folder) {
+        return;
+    }
+    fs.mkdir(folder, function(error) {
+        if (error && error.code !== 'EEXIST') {
+            throw error;
+        }
+    });
+}
+
+function addLogglyLogging() {
+    const logglySettings = logSettings.loggly;
+    if (logglySettings.disabled) {
+        return;
+    }
+    if (_.isNil(logglySettings.tags)) {
+        logglySettings.tags = [];
+    }
+    if (!_.isArray(logglySettings.tags)) {
+        throw new TypeError('Tags should be an array of strings');
+    }
+    if (logglySettings.tags.indexOf(packageJson.name) < 0) {
+        logglySettings.tags.push(packageJson.name);
+    }
+    logger.add(winston.transports.Loggly, _.omit(logglySettings, 'disabled'));
+}
+
+function addGraylogLogging() {
+    const logglySettings = logSettings.graylog;
+    if (logglySettings.disabled) {
+        return;
+    }
+    logger.add(WinstonGraylog2, _.omit(logglySettings, 'disabled'));
+}
+
+function overrideConsole() {
+    console.error = function() {
+        logger.error.apply(logger, formatArgs(arguments)); //level 0
+    };
+    console.warn = function() {
+        logger.warn.apply(logger, formatArgs(arguments)); //level 1
+    };
+    console.info = function() {
+        logger.info.apply(logger, formatArgs(arguments)); //level 2
+    };
+    console.log = function() {
+        logger.info.apply(logger, formatArgs(arguments)); //level 2
+    };
+    console.verbose = function() {
+        logger.verbose.apply(logger, formatArgs(arguments)); //level 3
+    };
+    console.debug = function() {
+        logger.debug.apply(logger, formatArgs(arguments)); //level 4
+    };
+    console.silly = function() {
+        logger.silly.apply(logger, formatArgs(arguments)); //level 5
+    };
+}
